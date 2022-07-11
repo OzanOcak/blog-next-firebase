b0d561c34487172f75bbbc46e11771a9f6ecb663
OzanOcak <ozanocak@gmail.com>
2022-07-11 18:35:44 -0400

    post cards

---
+++ components/PostCard.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import logo from "../public/logo.png";
+import { FiBookmark } from "react-icons/fi";
+const PostCard = () => {
+  return (
+    <div className="flex max-w-[46rem] h-[10rem] gap-[1rem] items-center cursor-pointer">
+      <div className="flex-[2.5] flex flex-col justify-content">
+        <div className="flex gap-[.5rem]">
+          <div className="grid place-items-center rounded-full overflow-hidden h-[1.5rem] w-[1.5rem]">
+            <Image
+              className="object-cover"
+              width={40}
+              height={40}
+              src={logo}
+              alt=""
+            />
+          </div>
+          <div className="font-semibold">Donald Duck</div>
+        </div>
+        <div className="font-bold text-2xl">
+          7 Free Tolls Make You More Productive in 2022
+        </div>
+        <div className="color-[#787878]">
+          Productivity skill that you can learn
+        </div>
+        <div className="flex items-center justify-between text-[#787878]">
+          <span>
+            June 15 . 5 min .{" "}
+            <span className="bg-[lightgray] p-1 rounded-full">
+              productivity
+            </span>{" "}
+          </span>
+          <span>
+            <FiBookmark className="h-5 w-5 cursor-pointer" />
+          </span>
+        </div>
+      </div>
+      <Image src={logo} height={100} width={100} alt="" />
+    </div>
+  );
+};
+export default PostCard;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner />
+      <div className="flex justify-center">
+        <div className="flex-1 max-w-7xl">
+          <div className="gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3 ">
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
