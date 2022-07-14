72e832d7cacba5eeac37bfa5b20f7ca1ae9603a4
OzanOcak <ozanocak@gmail.com>
2022-07-14 14:01:21 -0400

    fetching data and injecting to article main

--- components/ArticleMain.jsx
+++ components/ArticleMain.jsx
@@ -9,7 +9,8 @@ import { BiBookmarks } from "react-icons/bi";
 import { IoLogoTwitter } from "react-icons/io";
 import { FiMoreHorizontal } from "react-icons/fi";
 
-const ArticleMain = () => {
+const ArticleMain = ({ post, author }) => {
+  console.log(post, author);
   return (
     <div className="flex flex-[3]  items-center justify-center border-l border-r">
       <div className="h-screen overflow-scroll p-[2rem]">
@@ -17,17 +18,28 @@ const ArticleMain = () => {
           {/**-----------nav---------------- */}
           <div className="flex gap-[1rem] w-full ">
             <div className="w-[3rem] h-[3rem] grid  overflow-hidden rounded-full">
-              <Image className="object-cover" src={duck} alt="" />
+              <Image
+                className="object-cover"
+                width={50}
+                height={50}
+                src={`https://res.cloudinary.com/demo/image/fetch/${author?.data?.imageUrl}`}
+                alt=""
+              />
             </div>
             {/**-----------author ------------ */}
             <div className="flex-1 flex flex-col justify-center">
-              <div className="font-bold">Donald Duck</div>
+              <div className="font-bold">{author?.data?.name}</div>
               <div>
                 <span>
-                  June 15 . 7 min{" "}
-                  <span className="flex items-center gap-[.2rem] text-[green]">
+                  {new Date(post?.data?.postedOn).toLocaleString("en-US", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}{" "}
+                  . {post?.data?.postLength} min{" "}
+                  {/**<span className="flex items-center gap-[.2rem] text-[green]">
                     <AiFillAccountBook />
-                  </span>
+                </span>*/}
                 </span>
               </div>
             </div>
@@ -50,66 +62,18 @@ const ArticleMain = () => {
             <div className="flex flex-col gap-[1rem]">
               <Image className="object-cover" src={logo} alt="" />
             </div>
-            <h1 className="font-bold text-3xl">
-              How to learn JavaScript in hard way
-            </h1>
+            <h1 className="font-bold text-3xl">{post?.data?.title}</h1>
             <h3 className="font-blogSerifItalic text-[1.3rem] text-[#292929]">
-              Donald Duck, 20 July.2022
+              {author?.data?.name},{" "}
+              {new Date(post?.data?.postedOn).toLocaleString("en-US", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+              })}
             </h3>
           </div>
           <div className="font-blogSerif text-[1rem] text-[#292929]">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-            sapiente repudiandae, quam perspiciatis ab, consectetur eveniet
-            excepturi doloribus impedit beatae fuga exercitationem eligendi
-            obcaecati incidunt dicta itaque. Provident, hic assumenda?
+            {post?.data?.body}
           </div>
           {/** end of article container */}
         </div>
--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -1,12 +1,38 @@
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 import ArticleMain from "../../components/ArticleMain";
 import PostSideNav from "../../components/PostSideNav";
 import Recommendation from "../../components/Recommendation";
+import { BlogContext } from "../../context/blogContext";
 
 const Post = () => {
+  const { posts, users } = useContext(BlogContext);
+  const router = useRouter();
+  const [post, setPost] = useState([]);
+  const [author, setAuthor] = useState([]);
+
+  //console.log("------------>", posts);
+
+  useEffect(() => {
+    if (posts.legth === 0) return;
+
+    //posts.map((post) => console.log("post id:", post.id));
+    //console.log(router.query.slug);
+    // console.log(posts.find((post) => post.id === router.query.slug));
+
+    setPost(posts.find((post) => post.id === router.query.slug));
+    // console.log("-->", users);
+    //console.log(users.find((user) => user.id == post?.data?.author));
+    setAuthor(users.find((user) => user.id === post.data?.author));
+    //TypeError: Cannot read properties of undefined (reading 'author') -> use ?
+    //second error: post is set but author didnt get chance to be updated, so we need to give
+    // updating dependency will update after first automatic update -->[post] and delete
+    // the ? after post?.data? cz post will be empty in the first update
+  }, [post]); // useEffect calls in order to process once, not many times,didMount & didUpdate
   return (
     <div className="flex">
       <PostSideNav />
-      <ArticleMain />
+      <ArticleMain post={post} author={author} />
       <Recommendation />
     </div>
   );
